b05f8c288a11b94df4943d99a801a9d30c7b8464
AsifZaman777 <eramasif16@gmail.com>
2021-11-29 21:53:56 +0600

    Added Form 5

--- TravelGuide/Form5.cs
+++ TravelGuide/Form5.cs
@@ -20,31 +20,36 @@ namespace TravelGuide
         private void label2_Click(object sender, EventArgs e)
         {
 
-        }
+        }  //nothing
 
         private void label1_Click(object sender, EventArgs e)
         {
-
+            //nothing
         }
 
         private void panel4_Paint(object sender, PaintEventArgs e)
         {
-
+            //nothing
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-
+            //nothing
         }
 
         private void pictureBox9_Click(object sender, EventArgs e)
         {
-
+            //nothing
         }
 
         private void pictureBox6_Click(object sender, EventArgs e)
         {
+            //nothing
+        }
 
+        private void Food_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
