201294fbaa642e411344849d08df8cc61f019d95
AsifZaman777 <eramasif16@gmail.com>
2021-12-07 02:05:37 +0600

    added page

--- TravelGuide/Form1.Designer.cs
+++ TravelGuide/Form1.Designer.cs
@@ -50,111 +50,109 @@ namespace TravelGuide
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(0, -2);
+            this.panel1.Location = new System.Drawing.Point(-1, -2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(370, 599);
+            this.panel1.Size = new System.Drawing.Size(398, 645);
             this.panel1.TabIndex = 0;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(57, 60);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(249, 177);
+            this.pictureBox1.Size = new System.Drawing.Size(398, 234);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // label1
             // 
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(57, 252);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(75, 255);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(232, 31);
+            this.label1.Size = new System.Drawing.Size(228, 38);
             this.label1.TabIndex = 1;
             this.label1.Text = "Tour Guide BD";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Harlow Solid Italic", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(53, 539);
+            this.label2.Location = new System.Drawing.Point(30, 567);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(253, 22);
+            this.label2.Size = new System.Drawing.Size(344, 30);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Makes your travel easy and fun";
+            this.label2.Text = "We promise to make your tour easy";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial Black", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(580, 105);
+            this.label3.Font = new System.Drawing.Font("Arial Black", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(573, 86);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(278, 38);
+            this.label3.Size = new System.Drawing.Size(297, 41);
             this.label3.TabIndex = 1;
             this.label3.Text = "Admin Dashboard";
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.button1.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold);
+            this.button1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(524, 161);
+            this.button1.Location = new System.Drawing.Point(517, 160);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(381, 61);
+            this.button1.Size = new System.Drawing.Size(395, 56);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Change 1";
+            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.button2.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold);
+            this.button2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button2.Location = new System.Drawing.Point(524, 270);
+            this.button2.Location = new System.Drawing.Point(517, 280);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(381, 61);
+            this.button2.Size = new System.Drawing.Size(395, 56);
             this.button2.TabIndex = 3;
-            this.button2.Text = "Change3";
+            this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.button3.Font = new System.Drawing.Font("Arial Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button3.Location = new System.Drawing.Point(524, 380);
+            this.button3.Location = new System.Drawing.Point(517, 395);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(381, 61);
+            this.button3.Size = new System.Drawing.Size(395, 56);
             this.button3.TabIndex = 4;
-            this.button3.Text = "Change3";
+            this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = false;
             // 
             // button4
             // 
             this.button4.BackColor = System.Drawing.Color.Crimson;
-            this.button4.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button4.Location = new System.Drawing.Point(618, 522);
+            this.button4.Location = new System.Drawing.Point(636, 565);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(192, 37);
+            this.button4.Size = new System.Drawing.Size(164, 44);
             this.button4.TabIndex = 5;
-            this.button4.Text = "Log Out";
+            this.button4.Text = "Logout";
             this.button4.UseVisualStyleBackColor = false;
             // 
             // button5
             // 
-            this.button5.BackColor = System.Drawing.Color.MediumSlateBlue;
-            this.button5.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold);
+            this.button5.BackColor = System.Drawing.Color.Crimson;
+            this.button5.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button5.Location = new System.Drawing.Point(859, 13);
+            this.button5.Location = new System.Drawing.Point(839, 12);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(149, 40);
+            this.button5.Size = new System.Drawing.Size(164, 44);
             this.button5.TabIndex = 6;
             this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = false;
@@ -163,8 +161,7 @@ namespace TravelGuide
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1020, 597);
+            this.ClientSize = new System.Drawing.Size(1025, 642);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -173,7 +170,7 @@ namespace TravelGuide
             this.Controls.Add(this.label3);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Dashboard";
+            this.Text = "Form1";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
