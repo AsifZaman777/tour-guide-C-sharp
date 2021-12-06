8e154d621d6b3aa9a0cc8297727cb146a9c9bacf
20-42186-1@student.aiub.edu <20-42186-1@student.aiub.edu>
2021-12-07 02:33:26 +0600

    added

---
+++ TravelGuide/Admin2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TravelGuide
+{
+    public partial class Admin2 : Form
+    {
+        public Admin2()
+        {
+            InitializeComponent();
+        }
+    }
+}
