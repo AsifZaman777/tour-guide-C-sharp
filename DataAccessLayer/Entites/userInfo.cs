72804420cceef98e48c5fcd5f8457e997cd35276
senanik360 <aniksen360@gmail.com>
2021-12-07 01:32:08 +0600

    updated

---
+++ DataAccessLayer/Entites/userInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entites
+{
+    public class userInfo
+    {
+        private string name { get; set; }
+        private string email { get; set; }
+        private string pass { get; set; }
+        private string id { get; set; }
+        private string phone { get; set; }
+        private string gender { get; set; }
+    }
+}
